e914baed21ae2e0cebd2dd40ec39919946400c60
ysng-neko9 <ryoma.yasunaga@neko9.org>
2019-06-02 16:46:06 +0900

    Update Vagrantfile

--- ns3/scratch/chapter5-base.cc
+++ ns3/scratch/chapter5-base.cc
@@ -1,7 +1,7 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
  * Copyright (c) 2013 ResiliNets, ITTC, University of Kansas
- * Copyright (c) 2019 Yu Nakayama
+ * Copyright (c) 2019 Yu Nakayama, Ryoma Yasunaga
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
